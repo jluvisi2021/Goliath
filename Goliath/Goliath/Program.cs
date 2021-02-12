3ea3baf2aa46530afb0ecf6e4bc6a869885ed911
Jacob <65638832+jluvisi2021@users.noreply.github.com>
2021-02-12 11:35:37 -0800

    First Commit
    A first commit of the empty ASP.NET Core application. Will be transformed into MVC later.

---
+++ Goliath/Goliath/Program.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Goliath
+{
+
+    /*
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+   _____       _ _       _   _      
+  / ____|     | (_)     | | | |                         
+ | |  __  ___ | |_  __ _| |_| |__   
+ | | |_ |/ _ \| | |/ _` | __| '_ \  
+ | |__| | (_) | | | (_| | |_| | | | 
+  \_____|\___/|_|_|\__,_|\__|_| |_| 
+          Password Manager       
+
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    
+    -> By Jacob Luvisi
+    -> GitHub: https://github.com/jluvisi2021/Goliath
+    -> C# 9.0 - .NET Core 5
+
+    Goliath is an ASP.NET Core 5 based password manager based on the
+    MVC design pattern.
+    This is my first attempt at an ASP.NET Core project.
+
+    Planned Features:
+    - User Accounts
+    - Encryption
+    - Bootstrap CSS
+    - Microsoft Azure Database
+    - Themes
+    - 2 Factor Authentication
+    - Categories for secure information
+    */
+
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Goliath
  Goliath is an ASP.NET Core 5 (MVC-based) password & secrets manager with user accounts, authentication, and encryption.
+ When Goliath is first released for deployment then this README will be updated.
